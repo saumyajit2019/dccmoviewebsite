d54780d224f7acabd6eaf90cdd9a2c8fb2ade7de
saumyajit2019 <saumyajit99@gmail.com>
2021-06-21 16:38:46 +0530

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -5,7 +5,7 @@ session_start();
 //$password = "";
 //$dbname = "complexdb";
 
-$servername = "sql6.freemysqlhosting.net";
+$servername = "sql5.freemysqlhosting.net";
 $username = "sql6420440";
 $password = "2nACQE9ElP";
 $dbname = "sql6420440";
