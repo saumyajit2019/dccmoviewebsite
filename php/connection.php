72c356c596dbc7e4db9c4fde39a11ad3b191bdd3
saumyajit2019 <saumyajit99@gmail.com>
2021-06-21 16:47:30 +0530

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -7,7 +7,7 @@ session_start();
 
 $servername = "sql6.freemysqlhosting.net";
 $username = "sql6420440";
-$password = "2nACQE9ElP";
+$password = "2nACQE9EIP";
 $dbname = "sql6420440";
 
 $today = strtotime("Yesterday"); //???? Why won't it work for today
